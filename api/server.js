4d980ecec1acf1cbfa7b661f8d039749f4b5e551
Belén Arizaga <belen.arizaga@location-world.com>
2023-10-14 13:05:25 -0500

    ajustes socket

--- api/server.js
+++ api/server.js
@@ -5,13 +5,22 @@ const config = require('./config')
 const routes = require('./network/routes')
 const db = require('./db')
 
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
 var app = express()
 db( config.DB_URL )
 
 app.use( body_parser.json() )
 app.use( body_parser.urlencoded({extended: false}) )
+app.use('/', express.static('public'))
 
 routes( app )
+
+io.on('connection', function(socket){
+    console.log('Nuevo cliente conectado.')
+    socket.emit('mensaje', 'Bienvenido')
+})
 
 app.listen( config.PORT )
 console.log(`La aplicacion se encuentra arriba en http://localhost:${config.PORT}/`)
